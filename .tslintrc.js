ee7368f9bab9e69018dba7318951e0def618afcc
chenhao <302835677@qq.com>
2017-08-02 14:58:54 +0800

    增加tslint配置

--- .tslintrc.js
+++ .tslintrc.js
@@ -8,6 +8,14 @@ module.exports = {
         // 要求使用 let 或 const 而不是 var
         'no-var-keyword': true,
         // 优先使用 const，其次才是 let
-        'prefer-const': true
+        'prefer-const': true,
+        //if,for,do等必须加花括号
+        'curly': true,
+        //不允许行末出现空格
+        'no-trailing-whitespace': true,
+        //使用单引号
+        'quotemark': [true, 'single', 'avoid-escape', 'avoid-template'],
+        //强制使用空格
+        'whitespace': [true, 'check-branch', 'check-decl', 'check-operator', 'check-module', 'check-separator', 'check-type', 'check-typecast', 'check-preblock']
     }
 };
